3e9117c0a9e87b94a9647ed03f233e1824032b91
Gaylord Julien <g.j@mailbox.org>
2021-10-01 03:07:31 +0200

    test

--- src/index.js
+++ src/index.js
@@ -11,12 +11,15 @@ async function showContent() {
   for (var i = 0; i < content.length; i++) {
       var div = document.createElement("div");
       var img = document.createElement('img');
-      div.className = "col-12 col-md-6";
+      div.className = "pt-3 pb-3 pb-md-0 col-md-4 col-12 text-center";
       img.className = "img-fluid"
       img.src = apiUrl + 
-content[i].image?.formats.small.url;
-      div.innerHTML = content[i].title + ' - ' + content[i].description;
-      mainContainer.append(div, img);
+content[i].image?.url;
+img.width = content[i].image?.width;
+img.height = content[i].image?.height;
+      div.innerHTML = content[i].title;
+      mainContainer.appendChild(div);
+      div.append(img, content[i].description);
       }
   } catch (e) {
     console.log('Error', e);
