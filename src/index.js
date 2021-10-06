a20531583b9e13ff21bc484274be69e0edc9da25
Gaylord Julien <g.j@mailbox.org>
2021-10-07 00:13:24 +0200

    import grid

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import retrieveTodaysSpecials from "./queries/todaySpecials.js";
-
 import showSeasonsSpecials from "./showSeasonsSpecials";
 import showTodaysSpecials from "./showTodaysSpecials";
 import "./style.css";
+import "./grid.css"
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
 
 showTodaysSpecials();
 
 showSeasonsSpecials();
+
